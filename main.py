25dfdfb33b5cd9753f8818a9e7d2e0ba3d287556
Aleksey Vladimirovich <33750648+sithortodox@users.noreply.github.com>
2023-05-03 10:19:12 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from aiogram.contrib.middlewares.fsm import FSMMiddleware
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher import FSMContext
 from memory_storage import MemoryStorage
+from aiogram.dispatcher.filters import Text
+from telegram_handlers import start, choose_city, process_date
 
 from config import API_TOKEN, AVAILABLE_CITIES
 from database import create_tables, close_db
@@ -20,9 +22,9 @@ dp = Dispatcher(bot, storage=MemoryStorage())  # добавить storage=Memory
 dp.middleware.setup(LoggingMiddleware())
 dp.middleware.setup(FSMMiddleware())
 
-dp.register_message_handler(start, commands=["start"])
-dp.register_message_handler(choose_city, Text(equals=list(AVAILABLE_CITIES.keys()), ignore_case=True))
-dp.register_message_handler(process_date, regexp=r"^\d{4}-\d{2}-\d{2}$", state='*')
+dp.register_message_handler(start, Command("start"))
+dp.register_message_handler(choose_city, Text(equals=AVAILABLE_CITIES.keys(), ignore_case=True), state='*')
+dp.register_message_handler(process_date, state='date')
 
 create_tables()
 executor.start_polling(dp, skip_updates=True, on_shutdown=close_db)
